876bab841b641b778dd47cbb36d0517ee635ab19
saliansyah <dhafinsaliansyah@gmail.com>
2025-10-11 21:36:05 +0700

    tray page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("tray");
+  res.render("dashboard");
 });
 
 app.get("/sekolah", (req, res) => {

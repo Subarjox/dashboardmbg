7766f3392c42121fc96735d0f7e776d0e475308c
saliansyah <dhafinsaliansyah@gmail.com>
2025-11-17 11:00:07 +0700

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const bcrypt = require('bcrypt');
-const session = require('express-session');
-const { createClient } = require('@supabase/supabase-js');
-const { isAuthenticated, isSekolah } = require('./middleware/authmiddleware');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const { createClient } = require("@supabase/supabase-js");
+const { isAuthenticated, isSekolah } = require("./middleware/authmiddleware");
 
 const app = express();
 
@@ -21,13 +21,14 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-app.use(session({
-  secret: process.env.SECRET, 
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 app.use((req, res, next) => {
   const path = req.path.replace(/\/$/, ""); // hapus trailing slash
@@ -45,45 +46,40 @@ app.use((req, res, next) => {
   res.locals.user = req.session.user || null;
 
   res.locals.pageTitle = "Dashboard";
-  res.locals.pageBreadcrumb = [
-    { name: "Dashboard", href: "/dashboard" }
-  ];
+  res.locals.pageBreadcrumb = [{ name: "Dashboard", href: "/dashboard" }];
 
   next();
 });
 
 // Routes
-app.get('/', (req, res) => res.render('login'));
-app.get('/register', (req, res) => res.render('register'));
+app.get("/", (req, res) => res.render("login"));
+app.get("/register", (req, res) => res.render("register"));
 
-
-
-app.post('/register', async (req, res) => {
-  const { nama, email,username, password } = req.body;
+app.post("/register", async (req, res) => {
+  const { nama, email, username, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const { data, error } = await supabase
-    .from('users')
-    .insert([{nama, email, username, password: hashedPassword }])
+    .from("users")
+    .insert([{ nama, email, username, password: hashedPassword }])
     .select();
 
   if (error) {
     console.log(error);
-    return res.render('register', { error: error.message });
+    return res.render("register", { error: error.message });
   }
 
-  res.render('login', { message: 'Register berhasil! Silakan login.' });
+  res.render("login", { message: "Register berhasil! Silakan login." });
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-    //user_admin
+  //user_admin
   try {
-
     const { data: adminData, error: adminError } = await supabase
-      .from('users')
-      .select('*')
-      .eq('email', email)
+      .from("users")
+      .select("*")
+      .eq("email", email)
       .limit(1);
 
     if (adminError) throw adminError;
@@ -92,24 +88,24 @@ app.post('/login', async (req, res) => {
       const admin = adminData[0];
       const match = await bcrypt.compare(password, admin.password);
 
-      if (!match) return res.render('/', { error: 'Password salah!' });
+      if (!match) return res.render("/", { error: "Password salah!" });
 
       req.session.user = {
         id: admin.id,
         nama: admin.nama,
         email: admin.email,
-        role: 'admin',
+        role: "admin",
       };
 
-      console.log('✅ Login sebagai ADMIN:', admin.email);
-      return res.redirect('/dashboard');
+      console.log("✅ Login sebagai ADMIN:", admin.email);
+      return res.redirect("/dashboard");
     }
 
     //User_Sekolah
     const { data: sekolahData, error: sekolahError } = await supabase
-      .from('sekolah')
-      .select('*')
-      .eq('email_sekolah', email)
+      .from("sekolah")
+      .select("*")
+      .eq("email_sekolah", email)
       .limit(1);
 
     if (sekolahError) throw sekolahError;
@@ -118,100 +114,98 @@ app.post('/login', async (req, res) => {
       const sekolah = sekolahData[0];
       const match = await bcrypt.compare(password, sekolah.password_sekolah);
 
-      if (!match) return res.render('/', { error: 'Password salah!' },
-        console.log('pass salah')
-      );
+      if (!match)
+        return res.render(
+          "/",
+          { error: "Password salah!" },
+          console.log("pass salah")
+        );
 
       req.session.user = {
         id_sekolah: sekolah.id_sekolah,
         nama: sekolah.nama_sekolah,
         email: sekolah.email_sekolah,
-        role: 'sekolah',
+        role: "sekolah",
       };
 
-      console.log('✅ Login sebagai SEKOLAH:', sekolah.email_sekolah);
-      return res.redirect('/');
+      console.log("✅ Login sebagai SEKOLAH:", sekolah.email_sekolah);
+      return res.redirect("/");
     }
 
     //User_SPPG
     const { data: sppgData, error: sppgError } = await supabase
-      .from('satuan_gizi')
-      .select('*')
-      .eq('email_sppg', email) 
+      .from("satuan_gizi")
+      .select("*")
+      .eq("email_sppg", email)
       .limit(1);
 
     if (sppgError) throw sppgError;
 
     if (sppgData && sppgData.length > 0) {
-
       const sppg = sppgData[0];
       const match = await bcrypt.compare(password, sppg.password_sppg);
 
-      if (!match) return res.render('/', { error: 'Password salah!' },
-        console.log('pass salah')
-      );
-      
+      if (!match)
+        return res.render(
+          "/",
+          { error: "Password salah!" },
+          console.log("pass salah")
+        );
+
       req.session.user = {
         id_sppg: sppg.id_sppg,
         nama: sppg.nama_sppg,
         email: sppg.email_sppg,
-        role: 'sppg',
+        role: "sppg",
       };
 
-      console.log('✅ Login sebagai SPPG:', sppg.email_sppg);
-      return res.redirect('/');
+      console.log("✅ Login sebagai SPPG:", sppg.email_sppg);
+      return res.redirect("/");
     }
 
-    return res.render('login', { error: 'Akun tidak ditemukan!' });
-
+    return res.render("login", { error: "Akun tidak ditemukan!" });
   } catch (err) {
-    console.error(' Error saat login:', err);
-    res.render('login', { error: 'Terjadi kesalahan server.' });
+    console.error(" Error saat login:", err);
+    res.render("login", { error: "Terjadi kesalahan server." });
   }
 });
 
-<<<<<<< HEAD
 app.get("/analitik", (req, res) => {
   res.render("analitik");
-=======
+});
 
-const dashboardRoutes = require('./routes/dashboardroutes');
-app.use('/dashboard', dashboardRoutes);
+const dashboardRoutes = require("./routes/dashboardroutes");
+app.use("/dashboard", dashboardRoutes);
 
-const siswaRoutes = require('./routes/siswaroutes');
-app.use('/siswa', siswaRoutes);
+const siswaRoutes = require("./routes/siswaroutes");
+app.use("/siswa", siswaRoutes);
 
-const sekolahRoutes = require('./routes/sekolahroutes');
-app.use('/sekolah', sekolahRoutes);
+const sekolahRoutes = require("./routes/sekolahroutes");
+app.use("/sekolah", sekolahRoutes);
 
-const supplierRoutes = require('./routes/supplierroutes');  
-app.use('/supplier', supplierRoutes);
+const supplierRoutes = require("./routes/supplierroutes");
+app.use("/supplier", supplierRoutes);
 
-const sppgRoutes = require('./routes/sppgroutes');
-app.use('/sppg', sppgRoutes);
+const sppgRoutes = require("./routes/sppgroutes");
+app.use("/sppg", sppgRoutes);
 
-
-
-app.get('/tes', isSekolah, (req, res) => res.render('dashjour'));
-app.get('/laporan', isAuthenticated, (req, res) => res.render('laporan'));
-
+app.get("/tes", isSekolah, (req, res) => res.render("dashjour"));
+app.get("/laporan", isAuthenticated, (req, res) => res.render("laporan"));
 
 // LOGOUT
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
   req.session.destroy(() => {
-    res.redirect('/');
+    res.redirect("/");
   });
->>>>>>> 0256849fcaa88412c5f83766c73a0f07c72cb476
 });
 
 // handle 404
 app.use((req, res, next) => {
-  res.status(404).render('404', {
-    pageTitle: '404 - Halaman Tidak Ditemukan',
-    pageCrumb: 'Error 404'
+  res.status(404).render("404", {
+    pageTitle: "404 - Halaman Tidak Ditemukan",
+    pageCrumb: "Error 404",
   });
 });
-
 
 // Start server
 const PORT = process.env.PORT || 3000;
